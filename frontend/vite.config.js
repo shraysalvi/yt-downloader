fdb6e954a5247d29be9934dc98635ad4a3756f70
SHRAY SALVI <80808215+shraysalvi@users.noreply.github.com>
2025-04-13 08:45:08 +0000

    update env load

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
 
-// https://vite.dev/config/
+dotenv.config() // Loads .env into process.env
+
+const SOCKET_URL = process.env.VITE_SOCKET_URL || 'http://localhost:8000'
+
 export default defineConfig({
-  plugins: [react(),   tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     proxy: {
       '/socket.io': {
-        target: import.meta.env.VITE_SOCKET_URL || 'http://localhost:8000',
+        target: SOCKET_URL,
         ws: true,
       },
       '/api': {
-        target: import.meta.env.VITE_SOCKET_URL || 'http://localhost:8000',
+        target: SOCKET_URL,
         changeOrigin: true,
       },
       '/download': {
-        target: import.meta.env.VITE_SOCKET_URL || 'http://localhost:8000',
+        target: SOCKET_URL,
         changeOrigin: true,
         secure: false,
       },
-    }
-  }
+    },
+  },
 })
